7b11d5da667541707bc5cc9992bfd7395d13a809
Bhavana Nelakuditi <bhavana2341@gmail.com>
2016-12-15 19:46:30 +0530

    Add files via upload

---
+++ Person.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor
+
+/**
+ *
+ * @author Mohak
+ */
+public abstract class Person {
+    protected int age;
+    protected String Name,Rank,PhysicalDescription;
+    protected Long Id;
+    
+}
---
+++ useless_Admin.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+/**
+ *
+ * @author Mohak
+ */
+public class useless_Admin extends Person {
+    
+    public useless_Admin()
+    {
+        
+    }
+    void addRecord(String Type)
+    {
+        
+    }
+    void deleteRecord(String Type)
+    {
+        
+    }
+    void updateRecord(String Type)
+    {
+        
+    }
+}
